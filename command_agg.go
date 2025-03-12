70f952c9d63d6de73aaf1df94801f1662739b002
mjh1207 <mitchh1207@gmail.com>
2025-03-12 13:39:36 -0600

    aggregator command

--- command_agg.go
+++ command_agg.go
@@ -1,17 +1,58 @@
 package main
 
 import (
+	"blog-aggregator/internal/database"
 	"context"
+	"database/sql"
 	"fmt"
+	"time"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	url := "https://www.wagslane.dev/index.xml"
-	feed, err := fetchFeed(context.Background(), url)
-	if err != nil {
-		return fmt.Errorf("unable to fetch feed from %s - Error: %v", url, err)
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("expected argument: duration string")
 	}
 
-	fmt.Println(*feed)
+	interval, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("expected argument: duration string")
+	}
+
+	fmt.Printf("Collecting feeds every %v\n", interval)
+
+	ticker := time.NewTicker(interval)
+	for ; ; <-ticker.C {
+		err := scrapeFeeds(s)
+		if err != nil {
+			return fmt.Errorf("there was an error scraping the next feed")
+		}
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return err
+	}
+	err = s.db.MarkFeedFetched(context.Background(), database.MarkFeedFetchedParams{
+		UpdatedAt: time.Now().UTC(),
+		LastFetchedAt: sql.NullTime{time.Now().UTC(), true},
+		ID: nextFeed.ID,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Fetching feed from %s\n", nextFeed.Url)
+	feed, err := fetchFeed(context.Background(), nextFeed.Url)
+	if err != nil {
+		return fmt.Errorf("unable to fetch feed from %s - Error: %v", nextFeed.Url, err)
+	}
+
+	for _, item := range feed.Channel.Item {
+		fmt.Printf("- %s\n", item.Title)
+	}
+
 	return nil
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -15,3 +15,13 @@ SELECT * FROM feeds;
 
 -- name: GetFeedByUrl :many
 SELECT * FROM feeds WHERE url = $1; 
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET updated_at = $1, last_fetched_at = $2
+WHERE id = $3;
+
+-- name: GetNextFeedToFetch :one
+SELECT * from feeds
+ORDER BY last_fetched_at NULLS FIRST, id
+LIMIT 1;
